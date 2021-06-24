295b2c40ccaae66e4c20755b182aba0ef29d7f91
kandakov <timofei.clover@gmail.com>
2021-06-24 12:29:45 +0300

    Реализованы методы класса ProductDAO с использованием списка (List)

--- src/main/java/ru/kandakov/onlinestore/dao/ProductDAO.java
+++ src/main/java/ru/kandakov/onlinestore/dao/ProductDAO.java
@@ -4,11 +4,14 @@ import org.springframework.stereotype.Component;
 import ru.kandakov.onlinestore.dto.Product;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+
 @Component
 public class ProductDAO {
     private static long PRODUCT_COUNT;
     private List<Product> products = new ArrayList<>();
+
     {
         products = new ArrayList<>();
 
@@ -16,11 +19,46 @@ public class ProductDAO {
         products.add(1, new Product(++PRODUCT_COUNT, "ручка", 20));
         products.add(2, new Product(++PRODUCT_COUNT, "степлер", 50));
     }
+
     public List<Product> index() {
         return products;
     }
 
-    public Product show(int id) {
-        return products.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+
+    public Product show(long id) {
+        return findById(id);
+    }
+
+    public void save(Product product) {
+        products.add(product);
+    }
+
+    public void update(Product product) {
+        Product foundProduct = findById(product.getId());
+        if (foundProduct != null) {
+            foundProduct.setLabel(product.getLabel());
+            foundProduct.setPrice(product.getPrice());
+        }
+    }
+
+    public void delete(long id) {
+        Iterator<Product> iterator = products.iterator();
+        while (iterator.hasNext()) {
+            Product product = iterator.next();
+            if (product.getId().equals(id)) {
+                iterator.remove();
+            }
+        }
+    }
+    private Product findById(long id) {
+//      return products.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+        Iterator<Product> iterator = products.iterator();
+        while (iterator.hasNext()) {
+            Product product = iterator.next();
+            if (product.getId().equals(id)) {
+                return product;
+            }
+        }
+        return null;
     }
 }
