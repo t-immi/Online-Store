98a22bce7cef6b18921395b32063173346dc75e3
kandakov <timofei.clover@gmail.com>
2021-06-28 12:59:11 +0300

    ArrayList заменен на HashMap

--- src/main/java/ru/kandakov/onlinestore/dao/ProductDAO.java
+++ src/main/java/ru/kandakov/onlinestore/dao/ProductDAO.java
@@ -3,38 +3,35 @@ package ru.kandakov.onlinestore.dao;
 import org.springframework.stereotype.Component;
 import ru.kandakov.onlinestore.dto.Product;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.util.HashMap;
 
 @Component
 public class ProductDAO {
     private static long PRODUCT_COUNT;
-    private List<Product> products = new ArrayList<>();
+
+    HashMap<Long, Product> products = new HashMap<>();
 
     {
-        products = new ArrayList<>();
-
-        products.add(0, new Product(++PRODUCT_COUNT, "карандаш", 10));
-        products.add(1, new Product(++PRODUCT_COUNT, "ручка", 20));
-        products.add(2, new Product(++PRODUCT_COUNT, "степлер", 50));
+        products.put(0L, new Product(++PRODUCT_COUNT, "карандаш", 10));
+        products.put(1L, new Product(++PRODUCT_COUNT, "ручка", 20));
+        products.put(2L, new Product(++PRODUCT_COUNT, "степлер", 50));
     }
 
-    public List<Product> index() {
+    public HashMap<Long, Product> index() {
         return products;
     }
 
 
     public Product show(long id) {
-        return findById(id);
+        return products.get(id);
     }
 
     public void save(Product product) {
-        products.add(product);
+        products.put(product.getId(), product);
     }
 
     public void update(Product product) {
-        Product foundProduct = findById(product.getId());
+        Product foundProduct = products.get(product.getId());
         if (foundProduct != null) {
             foundProduct.setLabel(product.getLabel());
             foundProduct.setPrice(product.getPrice());
@@ -42,23 +39,7 @@ public class ProductDAO {
     }
 
     public void delete(long id) {
-        Iterator<Product> iterator = products.iterator();
-        while (iterator.hasNext()) {
-            Product product = iterator.next();
-            if (product.getId().equals(id)) {
-                iterator.remove();
-            }
-        }
+        products.remove(id);
     }
-    private Product findById(long id) {
-//      return products.stream().filter(person -> person.getId() == id).findAny().orElse(null);
-        Iterator<Product> iterator = products.iterator();
-        while (iterator.hasNext()) {
-            Product product = iterator.next();
-            if (product.getId().equals(id)) {
-                return product;
-            }
-        }
-        return null;
-    }
+
 }
