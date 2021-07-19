9475d79f8ed14aa960e3e7e00af76b243417ce4d
kandakov <timofei.clover@gmail.com>
2021-07-19 13:24:18 +0300

    Добавлен тест для CustomerController

--- src/test/java/TestControllerTests.java
+++ src/test/java/TestControllerTests.java
@@ -3,7 +3,6 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -11,26 +10,22 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import ru.kandakov.onlinestore.controllеr.CustomerController;
-import ru.kandakov.onlinestore.repository.CustomerRepository;
+import ru.kandakov.onlinestore.controllеr.TestController;
 
-@Import(CustomerController.class)
+@Import(TestController.class)
 @RunWith(SpringRunner.class)
-@WebMvcTest(CustomerController.class)
+@WebMvcTest(TestController.class)
 @ContextConfiguration(classes = {ApplicationRunner.class})
-public class CustomerControllerTests {
 
+public class TestControllerTests {
     @Autowired
     private MockMvc mockMvc;
-
-    @MockBean
-    private CustomerRepository customerRepository;
 
     @Test
     public void testForSuccessfulTransfer () throws Exception {
 
         MockHttpServletRequestBuilder builder = MockMvcRequestBuilders
-                .get("/users");
+                .get("/test");
 
         mockMvc.perform(builder)
                 .andExpect(MockMvcResultMatchers.status().isOk());
