3506a3ce295b9b1f998bae1a31af212134f5f62d
kandakov <timofei.clover@gmail.com>
2021-07-26 13:23:34 +0300

    в методе show getById(id) заменено на findById(id), в методе delete при удаление продукта теперь удаляеться его связь с корзиной

--- src/main/java/ru/kandakov/onlinestore/controllеr/ProductController.java
+++ src/main/java/ru/kandakov/onlinestore/controllеr/ProductController.java
@@ -4,46 +4,52 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import ru.kandakov.onlinestore.dto.Product;
 import ru.kandakov.onlinestore.repository.ProductRepository;
+import ru.kandakov.onlinestore.repository.ShoppingCartGoodsRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("/product")
 public class ProductController {
 
     private final ProductRepository productRepository;
+    private final ShoppingCartGoodsRepository shoppingCartGoodsRepository;
 
     @Autowired
-    public ProductController(ProductRepository productRepository) {
+    public ProductController(ProductRepository productRepository, ShoppingCartGoodsRepository shoppingCartGoodsRepository) {
         this.productRepository = productRepository;
+        this.shoppingCartGoodsRepository = shoppingCartGoodsRepository;
     }
 
-    @GetMapping("/product-catalog")
+    @GetMapping("/catalog")
     public List<Product> outputProducts() {
         return productRepository.findAll();
     }
 
-    @GetMapping("/product/read/{id}")
-    public Product show(@PathVariable long id) {
-        return productRepository.getById(id);
+    @GetMapping("/read/{id}")
+    public Optional<Product> show(@PathVariable long id) {
+        return productRepository.findById(id);
     }
 
-    @PutMapping("/product/create")
+    @PutMapping("/create")
     @ResponseBody
     public Product save(@RequestBody Product product) {
         productRepository.save(product);
         return product;
     }
 
-    @PatchMapping("/product/update")
+    @PatchMapping("/update")
     @ResponseBody
     public Product update(@RequestBody Product product) {
         productRepository.save(product);
         return product;
     }
 
-    @DeleteMapping("/product/delete")
+    @DeleteMapping("/delete")
     @ResponseBody
     public Product delete(@RequestBody Product product) {
+        shoppingCartGoodsRepository.deleteAll(product.getShoppingCartGoods());
         productRepository.delete(product);
         return product;
     }
